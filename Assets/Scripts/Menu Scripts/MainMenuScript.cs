d3bc7f2a226452fa1a53149cbe75988a33013472
Raphile14 <rllddalangin@gmail.com>
2020-10-16 19:34:29 +0800

    indev 1.0.0 (10/16/2020)
    Menu Changes

---
+++ Assets/Scripts/Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Switch to Create Lobby Scene
+    public void CreateButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Switch to Join Lobby Scene
+    public void JoinButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Switch to How To Play Scene
+    public void HowToPlayButton()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    // Quit Program
+    public void QuitGame()
+    {
+        Debug.Log("ApplicationQuit");
+        Application.Quit();
+    }
+}
